1bd23867a8204366d8bb23176bceeea79fa9b902
willsky <wangcong.m3m0ry@bytedance.com>
2024-05-16 20:32:53 +0800

    feat: add addr

--- ch3/src/entry.asm
+++ ch3/src/entry.asm
@@ -1,12 +1,12 @@
     .section .text.entry            # 定义代码段 entry
     .globl   _start                 # 定义全局符号
 _start:
-    la       sp, boot_stack_top
-    call     rust_main
+    la       sp, boot_stack_top     # 将 boot_stack_top 地址存入栈指针
+    call     rust_main              # 调用 rust_main
 
-    .section .bss.stack
-    .globl   boot_stack_lower_bound
+    .section .bss.stack             # 定义未初始化的 stack 全局变量
+    .globl   boot_stack_lower_bound # 定义全局符号 boot_stack_lower_bound
 boot_stack_lower_bound:
-    .space   4096 * 16
-    .globl   boot_stack_top
+    .space   4096 * 16              # 分配 16kb 内存
+    .globl   boot_stack_top         # 定义全局符号 boot_stack_top
 boot_stack_top:
